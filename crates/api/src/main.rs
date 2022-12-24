68c59451e38a0d2cf44dd118c1b6901c1273ac43
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-12-23 22:35:31 -0600

    feat: accept port from env

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -6,13 +6,19 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    // Get the port to listen on from the environment, default 8000
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|it| it.parse().ok())
+        .unwrap_or(8000);
+
     let app = Router::new()
         .route("/", get(hello))
         .route("/echo/", post(echo))
         .route("/echo/:name", get(echo_name))
         .route("/health", get(health));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
